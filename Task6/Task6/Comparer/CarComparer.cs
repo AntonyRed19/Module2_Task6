d37af3b4ff126e240fbe36a212c7f3867fd45ea5
DESKTOP-GCCS405\Antony <anton.redy19@gmail.com>
2021-03-10 10:31:33 +0200

    added CarComparer

---
+++ Task6/Task6/Comparer/CarComparer.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Task6.Models.Enums;
+using Task6.Models;
+
+namespace Task6.Comparer
+{
+    public class CarComparer : IComparer
+    {
+        public int Compare(object first, object second)
+        {
+            var x = first as KIndofLightCar;
+            var y = second as KIndofLightCar;
+
+            if (x.AirBags > y.AirBags)
+            {
+                return 1;
+            }
+            else if (x.AirBags < y.AirBags)
+            {
+                return -1;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+    }
+}
