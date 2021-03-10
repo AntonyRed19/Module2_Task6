916bc50c0aac2aadc1bea5d9fb8b29557b3064b3
DESKTOP-GCCS405\Antony <anton.redy19@gmail.com>
2021-03-10 11:03:56 +0200

    added CarExtention

---
+++ Task6/Task6/Helpers/CarExtention.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Task6.Models;
+
+namespace Task6.Helpers
+{
+        public static class CarExtention
+        {
+            public static KIndofLightCar[] FindCar(this ParkofCars park, double airbugs)
+            {
+                var temp = new KIndofLightCar[park.Cars.Length];
+                for (var i = 0; i < temp.Length; i++)
+                {
+                    var item = park.Cars[i];
+                    if (item.AirBags > airbugs)
+                    {
+                        temp[i] = park.Cars[i];
+                    }
+                }
+
+                var tempResult = ArrayHelper.CleanCapacity(temp);
+                var result = new KIndofLightCar[tempResult.Length];
+
+                for (var i = 0; i < result.Length; i++)
+                {
+                    result[i] = tempResult[i] as KIndofLightCar;
+                }
+
+                return result;
+            }
+        }
+}
